0abbd6fdc84b7fa523f52274ce619889cbeacd83
Gabes Mak <gabriel@pixelbreaker.com>
2019-07-31 22:28:08 +0100

    fix deprecating ReflectMetaData

--- src/common/guards/roles.decorator.ts
+++ src/common/guards/roles.decorator.ts
@@ -1,4 +1,3 @@
-// TODO change to use SetMetaData befire ReflectMetaData is deprecated
-import { ReflectMetadata } from '@nestjs/common';
+import { SetMetadata } from '@nestjs/common';
 
-export const Roles = (...roles: string[]) => ReflectMetadata('roles', roles);
+export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
